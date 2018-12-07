f494a16b4d4ed190b7ead215d8f665512c8dc9d4
Siddharth Kulkarni <1siddharthkulkarni007@gmail.com>
2018-12-07 12:53:29 +0530

    added singly_linked_lists

---
+++ lists/singly_linked_list/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Installing the header files...."
+mv singly_ll.h /usr/include
+mv singly_ll_func_def.h /usr/include
+echo "Installation complete...."
+echo "Use #include<singly_ll.h> to use the library."
---
+++ lists/singly_linked_list/singly_ll.h
@@ -0,0 +1,32 @@
+#ifndef SINGLY_LL
+#define SINGLY_LL
+
+using namespace std;
+
+template<class t>
+class node {
+	public :
+		t data;
+		node* next;
+};
+
+template<class t>
+class sll {
+	private:
+		node<t>* head;
+
+	public:
+		sll();
+		append(t val);
+		prepend(t val);
+		bool insert_pos(t val, long pos);
+		t pop();
+		t delete_first();
+		bool delete_val(t val);
+		display();
+		node<t>* get_head();
+};
+
+#include<singly_ll_func_def.h>
+
+#endif
